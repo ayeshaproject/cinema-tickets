b5eb4d1d139baf9c4efb177f4042e5ce015fcbf8
dasha <ad2029@york.ac.uk>
2025-03-08 23:47:48 +0000

    Added logger and exception handling

--- cinema-tickets-java/src/main/java/uk/gov/dwp/uc/pairtest/TicketServiceImpl.java
+++ cinema-tickets-java/src/main/java/uk/gov/dwp/uc/pairtest/TicketServiceImpl.java
@@ -1,5 +1,7 @@
 package uk.gov.dwp.uc.pairtest;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import thirdparty.paymentgateway.TicketPaymentServiceImpl;
 import thirdparty.seatbooking.SeatReservationServiceImpl;
 import uk.gov.dwp.uc.pairtest.domain.TicketTypeRequest;
@@ -11,6 +13,7 @@ public class TicketServiceImpl implements TicketService {
     /**
      * Should only have private methods other than the one below.
      */
+    private static final Logger log = LogManager.getLogger(TicketServiceImpl.class);
     private static final HashMap<TicketTypeRequest.Type, Integer> eachPrice = new HashMap<>();
     static {
         eachPrice.put(TicketTypeRequest.Type.ADULT, 25);
@@ -24,26 +27,44 @@ public class TicketServiceImpl implements TicketService {
     @Override
     public void purchaseTickets(Long accountId, TicketTypeRequest... ticketTypeRequests) throws InvalidPurchaseException {
 
-        validateAcctId(accountId);
-        validatePurchase(ticketTypeRequests);
+        try {
+            log.info("The validation checks begins");
+            validateAcctId(accountId);
+            validatePurchase(ticketTypeRequests);
 
-        int totalSeats = calculateSeats(ticketTypeRequests);
-        int totalPrice = calculatePrice(ticketTypeRequests);
+            log.info("Calculating the total seats and total price");
+            int totalSeats = calculateSeats(ticketTypeRequests);
+            int totalPrice = calculatePrice(ticketTypeRequests);
 
-        ticketPaymentService.makePayment(accountId, totalPrice);
-        seatReservationService.reserveSeat(accountId, totalSeats);
+            log.info("Making payment of amount {} for accountID {}", totalPrice, accountId);
+            ticketPaymentService.makePayment(accountId, totalPrice);
+            log.info("Reserving {} seats for accountID {}", totalSeats, accountId);
+            seatReservationService.reserveSeat(accountId, totalSeats);
+        }
+        catch (InvalidPurchaseException e) {
+            log.error("Invalid purchase for account ID {}, error: {}", accountId,e.getMessage());
+            throw e;
+        } catch(Exception e) {
+          log.error("Invalid process of purchasing ticket for accountID {}, error {}",accountId, e.getMessage(), e);
+          throw new RuntimeException("Purchase terminated due to error. Please try again", e);
+        }
+
 
     }
 
     private void validateAcctId(Long accountID) {
+        log.info("Validating accountID for {}", accountID);
         if(accountID <= 0) {
-            //throw exception
+            log.warn("Invalid account ID {}", accountID);
+            throw new InvalidPurchaseException("The accountID should be greater than 0");
         }
 
     }
 
+    // validation logic for ticket type and total number of tickets
     private void validatePurchase(TicketTypeRequest[] ticketTypeRequest) {
 
+        log.info("Validating the ticket type and total number of tickets");
         int adults = 0;
         int totalTickets = 0;
 
@@ -52,7 +73,8 @@ public class TicketServiceImpl implements TicketService {
             int ticketsCount = req.getNoOfTickets();
 
             if(ticketsCount < 0) {
-                //throw exception
+                log.warn("Invalid number of tickets : {}", ticketsCount);
+                throw new InvalidPurchaseException("The total tickets cannot be negative");
             }
 
             if(ticketType == TicketTypeRequest.Type.ADULT) {
@@ -62,10 +84,12 @@ public class TicketServiceImpl implements TicketService {
         }
 
         if(adults == 0) {
-            //throw exception
+            log.warn("Adult count is {}",adults);
+            throw new InvalidPurchaseException("Atleast one adult is needed with children and/or infants");
         }
         if(totalTickets> 25) {
-            //throw exception
+            log.warn("Max no. of tickets reached : {}", totalTickets);
+            throw new InvalidPurchaseException("Maximum no. of tickets purchased at a time cannot exceed 25");
         }
     }
 
@@ -77,6 +101,7 @@ public class TicketServiceImpl implements TicketService {
                totalSeats += req.getNoOfTickets();
             }
         }
+        log.debug("Total seats booked : {}", totalSeats);
         return totalSeats;
 
     }
@@ -89,6 +114,7 @@ public class TicketServiceImpl implements TicketService {
             price += ticketCount * eachPrice.get(ticketType);
         }
 
+        log.debug("Total price of the ticket : {}", price);
         return price;
 
     }
